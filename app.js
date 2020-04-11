b4d92f521e9d6bddc49c2219f8b1caceb9ca4bd8
Bhavya Patel <patel.bhavya@husky.neu.edu>
2020-04-11 15:35:38 -0400

    circleci integration and user api

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 const cors = require("cors");
+
+// Connect Database
+connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+// Define Routes
+app.use("/", require("./routes/user"));
+
+module.exports = app;
