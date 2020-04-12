7463df37d944f55ab1cb94d60de68c8222def0be
Bhavya Patel <patel.bhavya@husky.neu.edu>
2020-04-12 15:42:21 -0400

    Update a trip

--- routes/trip.js
+++ routes/trip.js
@@ -125,4 +125,95 @@ router.delete("/v1/trip/:id", auth, async (req, res) => {
   }
 });
 
+// @route    PUT /v1/trip/:id
+// @desc     Update Trip
+// @access   Private
+router.put(
+  "/v1/trip/:id",
+  auth,
+  [
+    check("trip_name", "Name is required").not().isEmpty(),
+    check("place_id", "Place ID is required").not().isEmpty(),
+    check("start_date")
+      .custom((value, { req }) => {
+        return moment(value, "YYYY-MM-DD", true).isValid();
+      })
+      .withMessage("Start Date is not valid"),
+    check("end_date")
+      .custom((value, { req }) => {
+        return moment(value, "YYYY-MM-DD", true).isValid();
+      })
+      .withMessage("End Date is not valid")
+      .custom((value, { req }) => {
+        return req.body.start_date <= value;
+      })
+      .withMessage("Start Date must come before End date"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { trip_name, place_id, start_date, end_date } = req.body;
+    try {
+      let trip = await Trip.findOne({ _id: req.params.id, user: req.user.id });
+
+      if (!trip) {
+        return res.status(404).json({ errors: [{ msg: "Trip not found" }] });
+      }
+
+      // Check if start date is before the actual start date
+      if (moment(start_date).isAfter(trip.start_date)) {
+        return res.status(400).json({
+          errors: [{ msg: "Start date is after the Actual trip start date" }],
+        });
+      }
+
+      // Check if end date is after the actual end date
+      if (moment(end_date).isBefore(trip.end_date)) {
+        return res.status(404).json({
+          errors: [{ msg: "End date is before the Actual trip end date" }],
+        });
+      }
+
+      let availableTrips = await Trip.find(
+        {
+          _id: {
+            $ne: req.params.id,
+          },
+          user: req.user.id,
+        },
+        "start_date end_date"
+      );
+
+      // Check whether there are any conflicts or not
+      availableTrips = availableTrips.filter((a) => {
+        return (
+          moment(start_date).isBetween(a.start_date, a.end_date, null, "[]") ||
+          moment(end_date).isBetween(a.start_date, a.end_date, null, "[]")
+        );
+      });
+
+      if (availableTrips.length > 0) {
+        return res
+          .status(400)
+          .json({ msg: "Your trip is conflicting with one of your trips" });
+      }
+
+      trip.trip_name = trip_name;
+      trip.place_id = place_id;
+      trip.start_date = start_date;
+      trip.end_date = end_date;
+
+      await trip.save();
+
+      return res.status(200).json();
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ errors: [{ msg: "Bad request" }] });
+    }
+  }
+);
+
 module.exports = router;
