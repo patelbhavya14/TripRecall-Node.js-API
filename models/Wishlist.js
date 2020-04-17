7da83088306fc6fb576c0bae05e0199126dc214e
Bhavya Patel <patel.bhavya@husky.neu.edu>
2020-04-17 12:19:21 -0400

    Delete Wishlist

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -12,7 +12,7 @@ const WishlistSchema = new mongoose.Schema(
     },
   },
   {
-    timestamps: { createdAt: "created_at" },
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
   }
 );
 
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -14,9 +14,22 @@ router.post(
   [check("place_id", "Place ID is required is required").not().isEmpty()],
   async (req, res) => {
     try {
-      let wishlist = Wishlist({
+      let wishlist = await Wishlist.findOne({
+        user: req.user.id,
         place_id: req.body.place_id,
       });
+
+      if (wishlist) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Wishlist already present" }] });
+      }
+
+      wishlist = Wishlist({
+        user: req.user.id,
+        place_id: req.body.place_id,
+      });
+
       await wishlist.save();
       return res.status(200).json();
     } catch (err) {
@@ -26,5 +39,22 @@ router.post(
     }
   }
 );
+
+// @route    DELETE /v1/wishlist/:id
+// @desc     Delete Wishlist
+// @access   Private
+router.delete("/v1/wishlist/:id", auth, async (req, res) => {
+  try {
+    let wishlist = await Wishlist.findById(req.params.id);
+
+    if (!wishlist) {
+      return res.status(404).json({ errors: [{ msg: "Wishlist not found" }] });
+    }
+
+    await wishlist.remove();
+
+    return res.status(200).json();
+  } catch (err) {}
+});
 
 module.exports = router;
