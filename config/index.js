52d86c099c878f198c49ba01bb6e98c68c39d96d
Bhavya Patel <patel.bhavya@husky.neu.edu>
2020-04-18 13:27:39 -0400

    environment variable for test environment

--- config/index.js
+++ config/index.js
@@ -5,6 +5,10 @@ const config = {
     mongoURI: "mongodb://localhost:27017/triprecall",
     jwtSecret: "secret",
   },
+  test: {
+    mongoURI: "mongodb://localhost:27017/triprecall",
+    jwtSecret: "secret",
+  },
   production: {
     mongoURI: process.env.DB_HOST,
     jwtSecret: process.env.JWT_SECRET,
