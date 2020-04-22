c056dcabc71f2208edd1b6212f47d6f4d4e0131d
Bhavya Patel <patel.bhavya@husky.neu.edu>
2020-04-22 16:15:34 -0400

    attraction update without note 1

--- routes/attraction.js
+++ routes/attraction.js
@@ -76,9 +76,11 @@ router.get("/v1/trip/:tripId/attractions", auth, async (req, res) => {
       return res.status(404).json({ errors: [{ msg: "Trip not found" }] });
     }
 
-    let attractions = await Attraction.find({ trip: req.params.tripId }).sort({
-      start_time: 1,
-    });
+    let attractions = await Attraction.find({ trip: req.params.tripId })
+      .sort({
+        start_time: 1,
+      })
+      .select("-note");
 
     return res.status(200).json(attractions);
   } catch (err) {
